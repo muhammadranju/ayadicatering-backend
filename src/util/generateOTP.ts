24003db35da744cb28d6f8fae06a46f59b898beb
Muhammad Ranju <mdranju23@gmail.com>
2026-01-31 03:28:57 +0600

    feat: Implement core authentication, user management, email utilities, set package module, and centralized configuration.

--- src/util/generateOTP.ts
+++ src/util/generateOTP.ts
@@ -3,3 +3,9 @@ const generateOTP = () => {
 };
 
 export default generateOTP;
+
+// const generateOTP = () => {
+//   return Math.floor(10000 + Math.random() * 90000);
+// };
+
+// export default generateOTP;
